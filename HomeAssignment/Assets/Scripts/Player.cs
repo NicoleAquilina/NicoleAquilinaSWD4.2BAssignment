8b0012175dbbe7af284a59a9e75bcf6512852c58
NicoleAquilina <73161826+NicoleAquilina@users.noreply.github.com>
2020-12-23 20:52:17 +0100

    Car Move left and right / ViewPortToWorldPoint()

---
+++ HomeAssignment/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float xMin, xMax, yMin, yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void SetUpMoveBoundaries()
+    {
+        //instance of the camera
+        Camera gameCamera = Camera.main;
+
+        //Boundaries
+        //xMin = 0 and xMax = 1
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x +padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        //yMin = 0 and yMax = 1
+        // yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        //yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+        
+    }
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal")* Time.deltaTime*moveSpeed;
+        var nexXPos =Mathf.Clamp( transform.position.x + deltaX,xMin,xMax);
+
+        transform.position = new Vector2(nexXPos, transform.position.y);
+    }
+}
