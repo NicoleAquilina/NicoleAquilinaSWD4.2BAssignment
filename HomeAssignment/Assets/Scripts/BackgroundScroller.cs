5c69a654b5de8ad090f888dbdabea7b393c471c7
NicoleAquilina <73161826+NicoleAquilina@users.noreply.github.com>
2020-12-26 09:04:52 +0100

    Scrolling Background and Rearranged assets folder

---
+++ HomeAssignment/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    //the speed of the scrolling
+    [SerializeField] float BackgroundScrollSpeed;
+    //The material from the texture
+    Material material;
+    //the movement
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get material of the background from the renderer component
+        material = GetComponent<Renderer>().material;
+        //will scroll in the y-axis at the speed given
+        offSet = new Vector2(0f, BackgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the material textureOffSet by the movement offset every frame
+        material.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
