77c0a5a022439fd1dbd2efd59b2de09390d3411b
NicoleAquilina <73161826+NicoleAquilina@users.noreply.github.com>
2020-12-29 20:43:16 +0100

    Removed and Implemented 2 paths
    -Removed the Scrolling Background
    - Implemented 2 paths (Currectly for same obstacle)
    -Created a Obstacle Wave Scriptable File

--- HomeAssignment/Assets/Scripts/ObstaclePathing.cs
+++ HomeAssignment/Assets/Scripts/ObstaclePathing.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ObstaclePathing : MonoBehaviour
 {
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] WaveConfig waveConfig;
+    int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        obstacleMove();
+    }
+
+    private void obstacleMove()
+    {
+        if(waypointIndex <= waypoints.Count -1)
+        {
+            //save the current waypoint in targetPosition
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            //to make sure z position is 0
+            targetPosition.z = 0f;
+            var movement = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movement);
+
+            //if we reached the trget waypoint
+            if(transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        //if enemy moved to all waypoints
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ HomeAssignment/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+
+    //the enemy
+    [SerializeField] GameObject obstaclePrefab;
+    //the path on which to go
+    [SerializeField] GameObject pathPrefab;
+    //time between each spawn
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    //include this random time difference between spawns
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    //number of enemies in the wave
+    [SerializeField] int numberOfEnemies = 3;
+    //enemy movement speed
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public GameObject GetObstaclePrefab()
+    {
+        return obstaclePrefab;
+    }
+
+    public List<Transform>GetWaypoints()
+    {
+        //eachh wave can have different waypoints
+        var waveWaypoints = new List<Transform>();
+        //go into the path prefab , and for each child ass it to the list
+        foreach(Transform child in pathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints;
+    }
+
+    public float GetTimeBetweenSpawns()
+    {
+        return timeBetweenSpawns;
+    }
+
+    public float GetSpawnRandomFactor()
+    {
+        return spawnRandomFactor;
+    }
+
+    public int GetNumberOfEnemies()
+    {
+        return numberOfEnemies;
+    }
+
+    public float GetEnemyMoveSpeed()
+    {
+        return enemyMoveSpeed;
+    }
+
+}
